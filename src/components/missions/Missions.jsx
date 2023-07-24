67fbece2eb78ca1bfb5fc4abeeb7f47675ab5905
Javier Grau <javiergrau.freelance@gmail.com>
2023-07-24 18:31:07 -0400

    [NOT WORKING YET] - Added 'Missions' component

--- src/components/missions/Missions.jsx
+++ src/components/missions/Missions.jsx
@@ -1,12 +1,33 @@
+import { list } from '../../redux/missions/missionsList';
+
+const MissionsList = () => {
+  const missions = [...list];
+  console.log(missions);
+  missions.map((mission) => {
+    <tr key={mission.mission_id}>
+      <td>{mission.mission_name}</td>
+      <td>{mission.description}</td>
+      <td>{(mission.membership = false)}</td>
+      <td>
+        <button type="button" className={(mission.joinMission = false)}>
+          Join Mission
+        </button>
+      </td>
+    </tr>;
+  });
+  return <tr />;
+};
+
 const Missions = () => (
   <section id="missions">
-    <table>
-      <tr>
+    <table id="missions__table">
+      <tr className="table__row-header">
         <th>Mission</th>
         <th>Description</th>
         <th>Status</th>
         <th> </th>
       </tr>
+      <MissionsList />
     </table>
   </section>
 );
