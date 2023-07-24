6b581e1de319df7908c51d71ae931ab5be1d8505
Javier Grau <javiergrau.freelance@gmail.com>
2023-07-24 17:22:08 -0400

    Added 'missionsList.js' and 'rocketList.js' as mocking API fetching

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import Missions from './components/missions/Missions';
 
 import './styles/App.css';
 
@@ -10,7 +11,7 @@ const App = () => (
     <Navbar />
     <Routes>
       <Route path="/" element={<h1>Rockets</h1>} />
-      <Route path="/missions" element={<h1>Missions</h1>} />
+      <Route path="/missions" element={<Missions />} />
       <Route path="/profile" element={<h1>My Profile</h1>} />
       <Route path="*" element={<h1>Not Found</h1>} />
     </Routes>
