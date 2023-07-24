eaa552f652ff4ff9fbb51aee7903ed1068f50c21
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-24 14:24:07 -0600

    Created 'MenuData-js' -file- and 'Navbar.js' -component- Setted the navbar for the main page.

---
+++ src/bd/MenuData.js
@@ -0,0 +1,18 @@
+const routes = [];
+
+routes.push({
+  to: '/',
+  text: 'Rockets',
+});
+
+routes.push({
+  to: '/missions',
+  text: 'Missions',
+});
+
+routes.push({
+  to: '/profile',
+  text: 'My Profile',
+});
+
+export default routes;
