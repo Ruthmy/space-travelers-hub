99259e6f2f0080f28c164f0524f449064f3b9c16
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-24 15:52:20 -0600

    Modified 'App.js' -component- Updated the routes and imports to add the Profile and Rockets components.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import Profile from './components/Profile';
+import Rockets from './components/Rockets';
 
 import './styles/App.css';
 
@@ -9,9 +11,9 @@ const App = () => (
   <HashRouter>
     <Navbar />
     <Routes>
-      <Route path="/" element={<h1>Rockets</h1>} />
+      <Route path="/" element={<Rockets />} />
       <Route path="/missions" element={<h1>Missions</h1>} />
-      <Route path="/profile" element={<h1>My Profile</h1>} />
+      <Route path="/profile" element={<Profile />} />
       <Route path="*" element={<h1>Not Found</h1>} />
     </Routes>
   </HashRouter>
