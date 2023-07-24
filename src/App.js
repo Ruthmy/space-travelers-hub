902334b0fd5a040e84dd581c4f17af959ecb7166
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-24 14:25:48 -0600

    Modified 'App.js' -component- Added the routes strutucre.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <></>
-  );
-}
+import Navbar from './components/Navbar';
+
+import './styles/App.css';
+
+const App = () => (
+  <HashRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<h1>Rockets</h1>} />
+      <Route path="/missions" element={<h1>Missions</h1>} />
+      <Route path="/profile" element={<h1>My Profile</h1>} />
+      <Route path="*" element={<h1>Not Found</h1>} />
+    </Routes>
+  </HashRouter>
+);
 
 export default App;
