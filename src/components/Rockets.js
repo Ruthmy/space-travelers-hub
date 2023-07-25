9927055f309eb0511473df03c9e12dba81dfcb9f
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-25 09:22:34 -0600

    Modified 'Rockets.js' -component- Moved to the component directory.

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,29 +1,27 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
-import rocketList from '../../redux/rockets/rocketsList';
-import { fetchRockets } from '../../redux/rockets/rocketsSlice';
+import { fetchRockets } from '../redux/rockets/rocketsSlice';
 import '../../styles/Rockets.css';
 
 const Rockets = () => {
   const dispatch = useDispatch();
-  /*
+
   // Get books from Redux store:
   const rocketsList = useSelector((state) => state.rockets);
-
   useEffect(() => {
     dispatch(fetchRockets());
   }, [dispatch]);
-  */
+
   return (
     <section className="rockets d-flex-column">
       <h2>Rockets</h2>
       {/* {rocketsList.loading && <div>Loading...</div>}
       {rocketsList.error && <div>{rocketsList.error}</div>} */}
       <div className="rockets__container d-flex-column">
-        {rocketList.map((rocket) => (
+
+        {rocketsList.rockets.map((rocket) => (
           <li key={rocket.id} className="rockets__list d-flex-row">
             <img src={rocket.flickr_images[0]} alt={rocket.name} />
             <div className="rockets__info d-flex-column">
@@ -33,6 +31,7 @@ const Rockets = () => {
             </div>
           </li>
         ))}
+
       </div>
     </section>
   );
