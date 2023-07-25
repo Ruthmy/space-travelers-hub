ea648223f2a88e33dec2a32824bcfa04debd7a20
Javier Grau <javiergrau.freelance@gmail.com>
2023-07-25 17:22:59 -0400

    Add fetching of data here

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,12 @@ import store from './redux/store';
 import App from './App';
 import './index.css';
 
+import { fetchRockets } from './redux/rockets/rocketsSlice';
+import { fetchMissions } from './redux/missions/missionsSlice';
+
+store.dispatch(fetchRockets());
+store.dispatch(fetchMissions());
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
