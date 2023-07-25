d4323c49e8475c89322296037699c2bc50e65004
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-25 09:24:53 -0600

    Modified 'rocketsSlice.js' -slice state- Added the AsyncThunk and its extraBuilders.

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,7 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { rocketsAPI } from '../../API/rocketsAPI';
+
+export const fetchRockets = createAsyncThunk(
+  'rockets/fetchRockets',
+  async () => {
+    try {
+      const response = await axios.get(rocketsAPI);
+      return response.data;
+    } catch (error) {
+      throw error.response;
+    }
+  },
+);
 
 const initialState = {
-  books: [],
+  rockets: [],
   status: 'idle',
   error: '',
 };
@@ -10,7 +24,21 @@ const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
   reducers: {},
-  extraReducers: {},
+  extraReducers(builder) {
+    builder
+      // Get rockets from API:
+      .addCase(fetchRockets.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchRockets.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.rockets = action.payload;
+      })
+      .addCase(fetchRockets.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default rocketsSlice.reducer;
