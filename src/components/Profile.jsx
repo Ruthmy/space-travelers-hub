364f4c8d44e1577bc3add5e446f730b044229e27
Ruthmy <Ruryemo.exe@gmail.com>
2023-07-25 19:08:14 -0600

    Modified 'Profile.jsx' -component- Added a filter function to render all the reserved rockets.

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,38 +1,43 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import '../styles/Profile.css';
 
-const Profile = () => (
-  <div className="profile">
-    <h1>My Profile</h1>
-    <div className="profile-container">
-      <div className="profile-info">
-        <h2>My Missions</h2>
-        <ul>
-          <li>SpaceX CRS-2</li>
-          <li>SpaceX CRS-3</li>
-          <li>SpaceX CRS-4</li>
-          <li>SpaceX CRS-5</li>
-          <li>SpaceX CRS-6</li>
-          <li>SpaceX CRS-7</li>
-          <li>SpaceX CRS-8</li>
-          <li>SpaceX CRS-9</li>
-          <li>SpaceX CRS-10</li>
-        </ul>
+const Profile = () => {
+  // Get rockets from Redux store:
+  const rocketsList = useSelector((state) => state.rockets);
+
+  return (
+    <div className="profile">
+      <h1>My Profile</h1>
+      <div className="profile-container">
+        <div className="profile-info">
+          <h2>My Missions</h2>
+          <ul>
+            <li>SpaceX CRS-2</li>
+            <li>SpaceX CRS-3</li>
+            <li>SpaceX CRS-4</li>
+            <li>SpaceX CRS-5</li>
+            <li>SpaceX CRS-6</li>
+            <li>SpaceX CRS-7</li>
+            <li>SpaceX CRS-8</li>
+            <li>SpaceX CRS-9</li>
+            <li>SpaceX CRS-10</li>
+          </ul>
+        </div>
+        <div className="profile-info">
+          <h2>My Rockets</h2>
+          <ul>
+            {rocketsList.rockets.filter((rocket) => rocket.reserved === true).map((rocket) => (
+              <li key={rocket.id}>{rocket.name}</li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="profile-info">
-        <h2>My Rockets</h2>
-        <ul>
-          <li>Falcon 1</li>
-          <li>Falcon 9</li>
-          <li>Falcon Heavy</li>
-        </ul>
-      </div>
+      <Link to="/" className="btn btn-primary">Back</Link>
     </div>
-    <Link to="/" className="btn btn-primary">Back</Link>
-  </div>
-);
+  );
+};
 
 export default Profile;
